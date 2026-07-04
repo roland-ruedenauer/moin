13dda978d71e36f02c40a8779d93bb2aae14e799
Roland Rüdenauer <roland.ruedenauer@gmail.com>
2026-07-04 14:41:06 +0200

    Add class moin.utils.Singleton

--- src/moin/utils/__init__.py
+++ src/moin/utils/__init__.py
@@ -23,6 +23,15 @@ from moin.error import ConfigurationError
 PICKLE_PROTOCOL = pickle.HIGHEST_PROTOCOL
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 #############################################################################
 # XML helper functions
 #############################################################################
